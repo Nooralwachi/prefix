f2dc40f587208818fdbba522952fc13c34806387
Nooralwachi <noor_alwachi@yahoo.com>
2021-05-09 20:39:05 -0700

    Update prefix_test.py

--- prefix_test.py
+++ prefix_test.py
@@ -1,5 +1,6 @@
 import unittest
 from prefix import *
+import io
 
 class Testword(unittest.TestCase):
     def test_roman_numeral(self):
@@ -8,7 +9,11 @@ class Testword(unittest.TestCase):
         self.assertEqual(find_common_prefix(['app', 'apple', 'book', 'banana', 'cook', 'coat','common','compare', 'coated']),'co')
         self.assertEqual(find_common_prefix2(['ax', 'apple', 'book', 'banana'] ),'a')
         self.assertEqual(find_common_prefix2(['app', 'apple', 'book', 'banana', 'cook', 'coat','common','compare', 'coated']),'co')
-        
+    
+    def testReadingOfInitialsFromFileStream(self):
+        testStream = io.StringIO()
+        testStream.write('grep\ngre\ngrenade \n')
+        testStream.seek(0)
+        self.assertEqual(['grep\n', 'gre\n', 'grenade \n'], readPrefixFromFileStream(testStream))
+
 unittest.main()
-
-
